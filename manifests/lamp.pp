2e48acd7df98d1e12658e1195231cab16fb630ee
Michael Anderson <manderso@nerdery.com>
2013-04-01 13:32:18 -0500

    Add configuration for lamp. Set apt-get update to run before main stage.

--- manifests/lamp.pp
+++ manifests/lamp.pp
@@ -1,16 +1,40 @@
 group { 'puppet': ensure => 'present' }
 
-include apt
-include vim
-include apache
-include apache::mod::php
-#include mysql
+
+stage { 'preinstall':
+  before => Stage['main']
+}
+
+class { 'apt':
+  always_apt_update => true,
+  stage => preinstall
+}
+
+class { 'apache': }
+class { 'apache::mod::php': }
 
 apache::vhost { 'localhost':
-priority => '10',
-port => '80',
-docroot => '/vagrant',
-logroot => '/vagrant/logs',
-serveraliases => 'localhost',
-configure_firewall => false,
+  priority => '10',
+  port => '80',
+  docroot => '/vagrant',
+  docroot_owner => 'vagrant',
+  docroot_group => 'vagrant',
+  logroot => '/vagrant/logs',
+  serveraliases => 'localhost',
+  configure_firewall => false,
 }
+
+class { 'mysql': }
+class { 'mysql::php': }
+class { 'mysql::server':
+config_hash => { 'root_password' => 'nerd' }
+}
+
+mysql::db { 'nerddb':
+  user => 'nerd',
+  password => 'nerd',
+  host => 'localhost',
+  grant => ['all'],
+}
+
+include vim
